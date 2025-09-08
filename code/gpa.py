8dbc4c859ffb7c8ec840a277aaa2af6ba24a0d77
mkaitoli <komann@syr.edu>
2025-09-07 23:06:05 -0400

    Fixing a typo

--- code/gpa.py
+++ code/gpa.py
@@ -3,7 +3,7 @@ This program takes a gpa as input and returns one of the following:
 
     "Invalid GPA" when gpa is outside the 0.0 to 4.0 range
     "Summa Cum Laude" when gpa >= 3.8
-    "Magna Cum Laude" when gpa >= 3.6 and gpa < 3.81
+    "Magna Cum Laude" when gpa >= 3.6 and gpa < 3.8
     "Cum Laude" when gpa >=3.4 and gpa < 3.6
     "Academic Probation" when gpa <= 1.8
     "Passing" for any other gpa
